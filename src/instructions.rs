6f91e79987c9f3c1e016f38984006f2baee8522c
Retsam <chess19@gmail.com>
2024-06-16 09:54:55 -0500

    Days since last unsafe: 0

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,20 +1,26 @@
-// Should probably not use an enum and just use a raw u8 for perf reasons, the Enum -> u8 lookup is unfortunate
-//  and probably adds to overhead for the v8 runner... but it's nice to have exhaustive checking for development
-//   (other than the TryFrom)
-// Can go back and remove it later and see if it's a perf win
+use std::mem::transmute;
+
+// Using an enum here is a little sketchy - it's nice to get exhaustive checking on the places I use this enum,
+//   but there's probably some overhead to the conversion and error checking, even though they're the same type
+//   under-the-hood even when doing some unsafe dark magic to be more efficient.
+// Might move this back to raw u8 later when I don't need the exhaustive checking so much.
 #[repr(u8)]
 pub enum Opcode {
     Return = 1,
     Constant = 2,
 }
+const OPCODE_MAX: u8 = (Opcode::Constant) as u8;
+
 impl TryFrom<u8> for Opcode {
     type Error = u8;
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
-        match value {
-            1 => Ok(Opcode::Return),
-            2 => Ok(Opcode::Constant),
-            other => Err(other),
+        // Using transmute here with bounds checking instead of the safer match value block
+        //  this logic is used in the vm loop, so minimizing overhead is probably a good idea
+        if value > 0 && value <= OPCODE_MAX {
+            unsafe { Ok(transmute(value)) }
+        } else {
+            Err(value)
         }
     }
 }
